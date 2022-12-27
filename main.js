ac6bd05f102f9758c75ad6a5bb3c66621f49c3c2
Muscab <Musabbasum7@gmail.com>
2022-12-27 11:16:27 +0000

    commit

--- main.js
+++ main.js
@@ -1,4 +1,31 @@
+//reference to the list input and button
 const list = document.querySelector("ul");
 const input = document.querySelector("input");
 const button = document.querySelector("button");
+
+//function that is will make the button responsive once clicked
+button.addEventListener("click", (e)=> {
+//clear the input field 
+
+// Create a new list item
+const item = document.createElement("li");
+item.textContent = input.value
+// Create a delete button for the list 
+const deleteButton = document.createElement("button");
+deleteButton.textContent="Delete";
+deleteButton.addEventListener("click", () => {
+    list.removeChild(item);
+})
+//append the children to the list item
+item.appendChild(deleteButton);
+list.appendChild(item);
+
+//prevents the form from being submited 
+    e.preventDefault();
+
+// Clear the input feild 
+input.value =" ";
+
+
+});
 
